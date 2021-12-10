a129457c83c0f73306bebaf85a0691829ab0735c
ChuckyMaster <67459259+ChuckyMaster@users.noreply.github.com>
2021-12-10 11:47:29 +0100

    exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique.classe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularExam';
+
+  stat1 = new Statistique(1462454, 'Mort du covid', 4545456456456, 'Million');
+  stat2 = new Statistique(7845475, 'Mort de la grippe', 4547656565, 'Millions');
 }
--- src/app/models/statistique.classe.ts
+++ src/app/models/statistique.classe.ts
@@ -1,2 +1,13 @@
 export class Statistique {
+  id: number;
+  title: string;
+  value: number;
+  unit: string;
+
+  constructor(id: number, title: string, value: number, unit: string) {
+    this.id = id;
+    this.title = title;
+    this.value = value;
+    this.unit = unit;
+  }
 }
