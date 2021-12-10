f946919e8574ee7512af3bac0247c0932b78f13d
ChuckyMaster <67459259+ChuckyMaster@users.noreply.github.com>
2021-12-10 14:08:11 +0100

    Exercice 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateStatComponent } from './create-stat/create-stat.component';
+import { DisplayStatComponent } from './display-stat/display-stat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'display', component: DisplayStatComponent },
+  { path: 'create', component: CreateStatComponent },
+  { path: '**', redirectTo: 'display' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiniatureStatComponent } from './miniature-stat/miniature-stat/miniature-stat.component';
+import { DisplayStatComponent } from './display-stat/display-stat.component';
+import { CreateStatComponent } from './create-stat/create-stat.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiniatureStatComponent
+    MiniatureStatComponent,
+    DisplayStatComponent,
+    CreateStatComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
