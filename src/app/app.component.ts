08567c9c022fcf7482fb707d8ed52bfa69cc4485
ChuckyMaster <67459259+ChuckyMaster@users.noreply.github.com>
2021-12-10 12:30:07 +0100

    exercice 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,32 @@ import { Statistique } from './models/statistique.classe';
 export class AppComponent {
   title = 'AngularExam';
 
-  stat1 = new Statistique(1462454, 'Mort du covid', 4545456456456, 'Million');
-  stat2 = new Statistique(7845475, 'Mort de la grippe', 4547656565, 'Millions');
+  tabStat: Statistique[] = [];
+
+  stat1 = new Statistique(
+    1462454,
+    'Mort du covid',
+    4545456456456,
+    'Millions de personnes'
+  );
+  stat2 = new Statistique(
+    7845475,
+    'Mort de la grippe',
+    4547656565,
+    'Millions de personnes'
+  );
+  stat3 = new Statistique(
+    4876131,
+    'Mort de la peste noir',
+    45454646,
+    'Milliers de personnes'
+  );
+
+  constructor() {
+    this.tabStat.push(this.stat1, this.stat2);
+
+    setTimeout(() => {
+      this.tabStat.push(this.stat3);
+    }, 5000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiniatureStatComponent } from './miniature-stat/miniature-stat/miniature-stat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiniatureStatComponent
   ],
   imports: [
     BrowserModule,
